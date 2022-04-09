65db1529e2e2d026c547f1fc544ef78a7bf42c33
maxblenner <maxb@live.ie>
2022-04-09 15:06:00 +0100

    Changing the class to more closely match the specification of the given question

--- CA_ONE.py
+++ CA_ONE.py
@@ -2,33 +2,37 @@
 
 class Employee(object):
 
-    #variables needed are: accNo, name, standard rate, overtime rate, tax credit, standard band
-    def __init__(self, accNo, name, stdRate, overRate, stdCut, taxCredit, stdBand) -> None:
-        self.__accNo = accNo
-        self.__name = name
-        self.__stdRate = stdRate
-        self.__overRate = overRate
-        self.__stdCut = stdCut #the point (hour) at which overtime pay kicks in
+    #variables needed are: self, staffID, fName, lName, regHours, hourlyRate, OTMulti, taxCredit, stdBand
+    def __init__(self, staffID, fName, lName, regHours, hourlyRate, OTMulti, taxCredit, stdBand):
+        self.__staffID = staffID
+        self.__fName = fName
+        self.__lName = lName
+        self.__regHours = regHours #the point (hour) at which overtime pay kicks in
+        self.__hourlyRate = hourlyRate
+        self.__OTMulti = OTMulti #the multiple by which overtime pay is calculated 
         self.__taxCredit = taxCredit
         self.__stdBand = stdBand
     
     def computePayment(self,hours):
-        if hours <= self.__stdCut: #if the employee did not work enough hours for overtime payment
+
+        overRate = self.__hourlyRate * self.__OTMulti
+
+        if hours <= self.__regHours: #if the employee did not work enough hours for overtime payment
             #separating standard and overtime hours 
             stdHours = hours
             overHours = 0
             
-        elif hours > self.__stdCut: #if the employeed worked enough hours for overtime pay
+        elif hours > self.__regHours: #if the employeed worked enough hours for overtime pay
             #separating standard and overtime hours
-            stdHours = self.__stdCut
-            overHours = hours - self.__stdCut
+            stdHours = self.__regHours
+            overHours = hours - self.__regHours
 
         #calculating standard and overtime pay individually
-        stdPay = stdHours * self.__stdRate
+        stdPay = stdHours * self.__hourlyRate
         if overHours == 0: #if there are no overtime hours to calculate, avoid multiplication of 0
             overPay = 0
         elif overHours > 0:
-            overPay = overHours * self.__overRate
+            overPay = overHours * overRate
 
         #print statements to ensure payment calcuations are working as intended
         print("Standard hours worked: ", stdHours)
@@ -36,7 +40,7 @@ class Employee(object):
         print("Overtime hours worked: ", overHours)
         print("Overtime pay: ", overPay) 
 
-e1 = Employee(12110,'John Greene', 15.45, 18.25, 35, 60.50, 700)
+e1 = Employee(12110,'Green', 'Joe', 35, 15.45, 1.5, 60.50, 700)
 
-e1.computePayment(35)
+e1.computePayment(37)
 
